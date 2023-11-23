b4a339f206d0b47bccb26cd61ab4ab956aa92791
Khon <merrimen03@gmail.com>
2023-11-23 09:51:31 +0700

    fixing

--- GD/Assets/Scripts/platformMoved.cs
+++ GD/Assets/Scripts/platformMoved.cs
@@ -1,45 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class platformMoved : MonoBehaviour
 {
     public bool forward = false;
-    public int vector = 1;
+    public float vector = 4f;
     private void FixedUpdate()
     {
         if (forward)
         {
-            if (vector == 1)
-            {
-                transform.position += new Vector3(4f, 0f, 0f) * Time.fixedDeltaTime;
-            }
-            else if (vector == 2)
-            {
-                transform.position -= new Vector3(4f, 0f, 0f) * Time.fixedDeltaTime;
-            }
+            transform.position += new Vector3(vector, 0f, 0f) * Time.fixedDeltaTime;
         }
         if (!forward)
         {
-            if (vector == 1)
-            {
-                transform.position += new Vector3(0f, 0f, 4f) * Time.fixedDeltaTime;
-            }
-            else if (vector == 2)
-            {
-                transform.position -= new Vector3(0f, 0f, 4f) * Time.fixedDeltaTime;
-            }
+            transform.position += new Vector3(0f, 0f, vector) * Time.fixedDeltaTime;
         }
     }
     private void OnCollisionEnter(Collision other)
     {
-        
-        vector = 2;
-        Debug.Log("1232131223");
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        vector = 2;
-        Debug.Log("1232131223");
+        if (other.collider.CompareTag("Graund"))
+        {
+            vector *= -1;
+            Debug.Log(vector);
+        }
+
+        if (other.collider.CompareTag("Player"))
+        {
+            if (forward)
+                other.gameObject.transform.position = new Vector3(transform.position.x, other.gameObject.transform.position.y,other.gameObject.transform.position.z);
+        }
     }
 }
