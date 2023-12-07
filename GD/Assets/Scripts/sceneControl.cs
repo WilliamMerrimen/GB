9da06a4e8bf205d5ed81bdbf64737cce9e040686
Sudeeneess <pleskovawaleria@gmail.com>
2023-12-08 00:18:36 +0500

    abstractSceneControl

---
+++ GD/Assets/Scripts/sceneControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class sceneControl : MonoBehaviour
+{
+    public void restartScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void mainActive() {
+        SceneManager.LoadScene(0);
+    }
+    public void sceneLoad( int sceneId) {
+        SceneManager.LoadScene(sceneId);
+    }
+
+ 
+}
